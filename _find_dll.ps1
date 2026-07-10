3836b7c7a5d5b6441ed08899baa2a670c58d11bc
Afrowave <sparenica@afrowave.ltd>
2026-07-10 20:31:42 +0200

    removed rubish

--- _find_dll.ps1
+++
@@ -1,6 +0,0 @@
-$pkg = "$env:USERPROFILE\.nuget\packages\oxyplot.avalonia_11_1"
-if (Test-Path $pkg) {
-    Get-ChildItem -Path $pkg -Recurse -Filter '*.dll' | Select-Object -First 10 | ForEach-Object { Write-Host $_.Name }
-} else {
-    Write-Host "Package not found at $pkg"
-}
